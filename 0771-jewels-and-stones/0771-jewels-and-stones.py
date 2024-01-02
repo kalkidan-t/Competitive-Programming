97e070deecdeadff2443fe237ce7b1e4aec6e5fc
Kalkidan Belayneh Debas <102314154+kalikal1@users.noreply.github.com>
2024-01-02 09:17:17 +0300

    Time: 70 ms (5.07%), Space: 17.3 MB (6.05%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+
+        jewel_count = 0
+        for stone in stones:
+            if stone in jewels:
+                jewel_count += 1
+
+        return jewel_count
